30fa894de0e54b9bcfb24489fde6b02f19b38d10
Monte Jiran <montejiran@gmail.com>
2017-05-15 20:52:02 -0700

    implement navbar with demo login and logout

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,13 +3,14 @@ import { Provider } from 'react-redux';
 import {Route, Redirect, Switch, Link, HashRouter} from 'react-router-dom';
 import SessionFormContainer from './session_form/session_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import NavBarContainer from './navbar/navbar_container';
 
 
 const App = () => (
   <div>
-    <h1>Super Scrolls</h1>
-      <AuthRoute path="/login" component={SessionFormContainer} />
-      <AuthRoute path="/signup" component={SessionFormContainer} />
+    <Route path="/" component={NavBarContainer}/>
+    <AuthRoute path="/login" component={SessionFormContainer} />
+    <AuthRoute path="/signup" component={SessionFormContainer} />
   </div>
 );
 
---
+++ frontend/components/navbar/navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class Navbar extends React.Component {
+
+  handleDemo(e) {
+    e.preventDefault();
+    this.props.login({username: "Monte", password:"password"});
+  }
+
+  handleLogout(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
+
+
+
+  render() {
+    return (
+      <nav className='navbar'>
+        <span className="NavTitle">SuperScrolls</span>
+        <button onClick={this.handleDemo.bind(this)} type="button">Demo</button>
+        <Link to="/login">
+          <button type="button" className='Login Button'>Login</button>
+        </Link>
+        <Link to="/signup">
+          <button type="button" className="Signup Button">Sign Up</button>
+        </Link>
+        <button onClick={this.handleLogout.bind(this)} type="button" className="Logout Button">Logout</button>
+      </nav>
+    );
+  }
+}
+
+export default withRouter(Navbar);
