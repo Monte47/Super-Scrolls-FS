f5d2eb8c0a8318b229894ca49db68341829d9999
Monte Jiran <montejiran@gmail.com>
2017-05-17 19:41:55 -0700

    add blur and title to index show page on hover

--- frontend/components/books_index/books_index_item.jsx
+++ frontend/components/books_index/books_index_item.jsx
@@ -3,9 +3,10 @@ import { Route, Link, withRouter } from 'react-router-dom';
 
 const BooksIndexItem = ({ book }) => (
   <li className="books-index-item">
-    <figure>
+    <figure className="book-index-item-figure">
       <Link to={`/books/${book.id}`}>
         <img className="books-index-picture" src={book.image_url} alt={book.title} />
+        <div className="book-index-item-title">{book.title}</div>
       </Link>
     </figure>
   </li>
