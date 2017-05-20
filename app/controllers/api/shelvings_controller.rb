537c76c1ac1f8a857f79356737d7b2528a65ceeb
Monte Jiran <montejiran@gmail.com>
2017-05-19 21:51:41 -0700

    create shelving controller

--- app/controllers/api/shelvings_controller.rb
+++ app/controllers/api/shelvings_controller.rb
@@ -1,15 +1,22 @@
 class Api::ShelvingsController < ApplicationController
 
   def create
-
+    @shelving = Shelving.new(shelving_params)
+    if @shelving.save
+      @book = Book.find(params[:shelve][:book_id])
+      render 'api/books/show'
+    else
+      render json: @shelving.errors.full_messages, status: 422
   end
 
   def destroy
-
+    @shelving = Shelving.find(params[:id])
+    @shelving.destroy
+    render json: @shelving
   end
 
   def shelving_params
-
+    params.require(:shelving).permit(:book_id, :bookshelf_id)
   end
-  
+
 end
