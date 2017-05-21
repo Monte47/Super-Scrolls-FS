938f7c1fc2eebc89190a2b44c591ba3bbbfe5f15
Monte Jiran <montejiran@gmail.com>
2017-05-21 13:11:14 -0700

    add uniqueness to review for user scope and book

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   validates :user_id, :book_id, :body, presence: true
+  validates :user_id, uniqueness: { scope: :book_id }
 
   belongs_to :user
   belongs_to :book
