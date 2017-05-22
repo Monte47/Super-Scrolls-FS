9230447d7ebbeb743c6b3508cbfad33f84e5ef9f
Monte Jiran <montejiran@gmail.com>
2017-05-22 11:09:18 -0700

    shelving now renders immediatley after created on book detail page

--- frontend/actions/shelvings_actions.js
+++ frontend/actions/shelvings_actions.js
@@ -1,8 +1,10 @@
 import * as APIUtil from '../util/shelving_api_util';
 import { requestBookshelf } from './bookshelf_actions';
+import { requestBook } from './book_actions';
 
 export const createShelving = shelving => dispatch => {
-  return APIUtil.createShelving(shelving);
+  return APIUtil.createShelving(shelving)
+  .then(book => dispatch(requestBook(book.id)));
 };
 
 export const destroyShelving = shelving => dispatch => {
